0261279609c6ebccc7de6a42457920349c01807d
Abdelrahmans00 <123298652+Abdelrahmans00@users.noreply.github.com>
2025-03-14 22:10:47 +0200

    i think last one

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,13 @@ import MemoryGame from "./MemoryGame";
 import SnakeGame from "./SnakeGame";
 import ConnectFour from "./ConnectFour";
 
-
-
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/game/memory" element={<MemoryGame />} />
-      <Route path="/game/snakegame" element={<SnakeGame/>}/>
-      <Route path="/game/connect4" element={<ConnectFour/>}/>
+      <Route path="/game/snakegame" element={<SnakeGame />} />
+      <Route path="/game/connect4" element={<ConnectFour />} />
     </Routes>
   );
 }
